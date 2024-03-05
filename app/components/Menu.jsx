c7a99ba93cc15425e990dec406983dd5781ba175
Angelogama21 <ang21gm33@gmail,com>
2024-03-05 09:50:19 -0600

    Tercer entrega

--- app/components/Menu.jsx
+++ app/components/Menu.jsx
@@ -1,13 +1,13 @@
 const Menu = () => {
     return (
-        <nav class="bg-gray-800 p-4">
-            <div class="container mx-auto flex justify-between items-center">
-                <a href="./" class="text-white text-lg font-bold">Gama 1</a>
+        <nav>
+            <div>
+                <a href="./">Gama 1</a>
 
             <div class="space-x-4">
-                <a href="./home" class="text-white hover:text-gray-300">Home</a>
-                <a href="./acerca" class="text-white hover:text-gray-300">Acerca de</a>
-                <a href="./contador" class="text-white hover:text-gray-300">Contador</a>
+                <a href="./home">Home</a>
+                <a href="./acerca">Acerca de</a>
+                <a href="./contador">Contador</a>
             </div>
             </div>
         </nav>
